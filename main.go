d10175d81080c0f61d67af65c0a483f2593b06fd
Mike Harp <msharp185@gmail.com>
2022-04-09 13:51:15 -0400

    Page templating stubbed out, 404s and 500s returned

--- main.go
+++ main.go
@@ -3,37 +3,69 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 type RenderContext struct {
 	TargetYear int
 }
 
+// ToDo - don't require .html at the end of page URLs
+// ToDo - ENVVAR for maintenance mode
 func main() {
+	// Declare rendering context variables
 	pageContext := &RenderContext{
 		TargetYear: 2023,
 	}
 
-	templates := template.Must(template.ParseFiles("templates/home.html"))
-
 	// Handle static asset requests
-	http.Handle(
-		"/static/",
-		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))),
-	)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Handle other requests
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if err := templates.ExecuteTemplate(w, "home.html", pageContext); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	layoutTplPath := filepath.Join("templates", "layout.html")
+	http.HandleFunc("/", handlePageRequests(layoutTplPath, pageContext))
 
 	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
 	//Print any errors from starting the webserver using fmt
-	println("Listening on port 8080")
+	log.Println("Listening on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		println(fmt.Errorf("stopped listening with error: %w", err).Error())
+		log.Panic(fmt.Errorf("stopped listening with error: %w", err).Error())
 	}
+}
+
+func handlePageRequests(layoutTplPath string, pageContext *RenderContext) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		targetTplPath := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+		if is404(targetTplPath) {
+			http.NotFound(w, r)
+			return
+		}
+
+		tmpl, err := template.ParseFiles(layoutTplPath, targetTplPath)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := tmpl.ExecuteTemplate(w, "layout", pageContext); err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func is404(targetTplPath string) bool {
+	info, err := os.Stat(targetTplPath)
+	if err != nil && os.IsNotExist(err) {
+		return true
+	}
+	if info.IsDir() {
+		return true
+	}
+	return false
 }
