32f6b53a37266e0bc63f9498b26ce96c34b06c72
Mike Harp <msharp185@gmail.com>
2023-05-31 23:52:50 -0400

    Add more info page content, need styling and more work

--- js/info.js
+++ js/info.js
@@ -1,25 +1,44 @@
 $(document).ready(function() {
     let $countdownHolder = $("#countdown");
-
-    // TODO: check timezone on this...
     let countDownDate = new Date("Oct 7, 2023 17:30:00").getTime();
 
-    // Update the countdown every 1 second
-    let x = setInterval(function() {
+    let pluralize = function(noun, val) {
+        if (val === 1) {
+            return " " + val + " " + noun + " ";
+        }
+        return " " + val + " " + noun + "s ";
+    };
+
+    let setCountDown = function() {
         let now = new Date().getTime();
         let diff = countDownDate - now;
+        if (diff < 0) {
+            $countdownHolder.text("Party Time!");
+            clearInterval(x);
+            return;
+        }
 
         let days = Math.floor(diff / (1000 * 60 * 60 * 24));
         let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-        $countdownHolder.text(days + " days " + hours + " hours " + minutes + " minutes and " + seconds + " seconds");
-
-        if (diff < 0) {
-            clearInterval(x);
-            $countdownHolder.css("visibility", "visible");
-            $countdownHolder.text("Party Time!");
+        let toSet = "";
+        if (days > 0) {
+            toSet += pluralize("day", days);
         }
-    }, 1000);
+        if (hours > 0) {
+            toSet += pluralize("hour", hours);
+        }
+        if (minutes >0) {
+            toSet += pluralize("minute", minutes);
+        }
+        toSet += pluralize("second", seconds);
+        $countdownHolder.text(toSet);
+    };
+
+    // Run setCountDown immediately
+    setCountDown();
+    // Update the countdown every 1 second
+    let x = setInterval(setCountDown, 1000);
 });
